a79cd638132dd36e4459a62fa8ee18a52cb6ff24
Arcylisz <olek1305@gmail.com>
2023-03-20 20:34:46 +0100

    update readme, book (bad composition)

--- AboutMe/resources/views/book/index.blade.php
+++ AboutMe/resources/views/book/index.blade.php
@@ -1,26 +1,39 @@
 @extends('layouts.app')
 @section('title', $viewData['title'])
 @section('content')
-
-<div class="row">
-    <div class="col-md-6 col-lg-4 mb-2">
-        <p>Daniel Correa – Paola Vallejo - Practical Laravel Develop clean MVC web applications</p>
+    <div class="container my-4">
+        <div class="row">
+            <div class="col-md-6 col-lg-4 mb-2">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top img-fluid rounded" src="{{ asset('image\books\PracticalLaravelMVC.webp') }}"
+                        alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Practical Laravel Develop clean MVC web applications</h5>
+                        <p class="card-text">Daniel Correa – Paola Vallejo.
+                        </p>
+                        <div class="justify-content-center d-flex">
+                            <a href="https://www.amazon.com/Practical-Laravel-Develop-clean-applications/dp/B09S69MHRB"
+                                class="btn btn-primary">Link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-2">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top img-fluid rounded" src="{{ asset('image\books\PracticalLaravelMVC.webp') }}"
+                        alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Practical Laravel Develop clean MVC web applications</h5>
+                        <p class="card-text">Daniel Correa – Paola Vallejo.
+                        </p>
+                        <div class="justify-content-center d-flex">
+                            <a href="https://www.amazon.com/Practical-Laravel-Develop-clean-applications/dp/B09S69MHRB"
+                                class="btn btn-primary">Link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="col-md-6 col-lg-4 mb-2">
-        
-    </div>
-    <div class="col-md-6 col-lg-4 mb-2">
-        
-    </div>
-    <div class="col-md-6 col-lg-4 mb-2">
-        
-    </div>
-    <div class="col-md-6 col-lg-4 mb-2">
-        
-    </div>
-    <div class="col-md-6 col-lg-4 mb-2">
-        
-    </div>
-</div>
 
 @endsection
